496834dc5600c61baae03a572df53233e6ab945d
Edward <createdliving1000@gmail.com>
2023-01-24 12:03:50 +0000

    modify converter class to read request body and deserialize it during post

---
+++ Converters/WalletJsonConverter.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Hubtel.Wallets.Api.Models;
+
+namespace Hubtel.Wallets.Api.Converters
+{
+ 
+    public class WalletConverter : JsonConverter<Wallet>
+    {
+        public override Wallet Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var json = JsonSerializer.Deserialize<JsonElement>(ref reader);
+            var accountNumber = json.GetProperty("AccountNumber").GetString();
+            var owner = json.GetProperty("Owner").GetString();
+            var name = json.GetProperty("Name").GetString();
+            var wallet = new Wallet(accountNumber);
+            wallet.Owner = owner;
+            wallet.Name = name;
+            return wallet;
+        }
+
+        public override void Write(Utf8JsonWriter writer, Wallet value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteString("AccountNumber", value.AccountNumber);
+            writer.WriteString("Type", value.Type.ToString());
+            writer.WriteString("Scheme", value.Scheme.ToString());
+            writer.WriteString("CreatedAt", value.CreatedAt.ToString());
+            writer.WriteString("Owner", value.Owner);
+            writer.WriteString("Name", value.Name);
+            writer.WriteEndObject();
+        }
+    }
+
+}
